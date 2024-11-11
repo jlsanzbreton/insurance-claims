fc0ae4b8ce2a544349156d6a0d1ee9c4cb1b5a0a
jlsanzbreton <jlsanzbreton@gmail.com>
2024-11-12 00:35:55 +0100

    chore: update package dependencies and tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/c
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
